61abe64ee803d1ad82a2c2af04bdfb8528462421
DanielCainelli <147373591+PaneAzzimo@users.noreply.github.com>
2026-01-30 13:25:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,42 @@
 <?php
 session_start();
-include "db.php";
+$conn = mysqli_connect("localhost", "root", "", "todo_app");
 
-if (isset($_POST["username"]) && isset($_POST["password"])) {
-
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    include "login_query.php";
+if (!isset($_SESSION["utente_id"])) {
+    header("Location: login.php");
+    exit;
 }
+
+$id_utente = $_SESSION["utente_id"];
+
+if (isset($_POST["nuova_task"])) {
+    $testo = $_POST["nuova_task"];
+    mysqli_query($conn, "INSERT INTO attivita (testo, id_utente) VALUES ('$testo', $id_utente)");
+}
+
+$risultato = mysqli_query($conn, "SELECT * FROM attivita WHERE id_utente = $id_utente");
 ?>
 
 <!DOCTYPE html>
 <html>
 <body>
 
-<h2>Login</h2>
+    <h1>Ciao <?= $_SESSION["username"] ?></h1>
+    <a href="logout.php">Logout</a>
 
-<form method="POST">
-    <input type="text" name="username">
-    <br><br>
-    <input type="password" name="password">
-    <br><br>
-    <button>Login</button>
-</form>
+    <form method="POST">
+        <input type="text" name="nuova_task" required>
+        <button type="submit">Aggiungi</button>
+    </form>
+
+    <ul>
+        <?php while($row = mysqli_fetch_assoc($risultato)): ?>
+            <li>
+                <?= $row["testo"] ?> 
+                <?php if($row["completata"]) echo "(Fatto)"; ?>
+            </li>
+        <?php endwhile; ?>
+    </ul>
 
 </body>
 </html>
