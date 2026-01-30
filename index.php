981e4ae19aab6e1c7474ba8b3bb99ca6c58ed1ef
DanielCainelli <147373591+PaneAzzimo@users.noreply.github.com>
2026-01-30 13:23:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,27 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
 
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+    body {
+        font-family: Arial;
+        background: #eee;
+    }
+
+    form {
+        width: 200px;
+        margin: 100px auto;
+        padding: 10px;
+        background: white;
+        border: 1px solid #ccc;
+    }
+
+    input, button {
+        width: 100%;
+        margin-top: 5px;
+    }
+</style>
+</head>
 <body>
 
 <h2>Login</h2>
